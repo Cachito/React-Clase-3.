2e98508423db65eb4a6105d988b772493da232cb
Cacho Carro <lcarro@globalnewsgroup.com>
2021-09-24 19:41:01 -0300

    primeir commit 3° clase

---
+++ index.js
@@ -0,0 +1,46 @@
+
+
+console.log("aguante motorhead");
+
+var provincia = "Buenos Aires";
+var nombre = "Sala B";
+var apellido = "Street Rock" // string o cadena de textp
+var suenaBien = true; // boolean
+var cache = 1234.5
+
+let edad = 25 // numerico o number o integer
+let origen = "mataderos";
+
+const PAIS = "Argentina"; // no cambia
+const PI = 3.14151926; // float o flotante o double (decimales)
+
+// console.log puede mostrar información, pero no debe usasrse para el usuario final
+console.log("Mi banda es " + nombre + " que hace " + apellido + " y es de " + provincia);
+
+// operaciones matemáticas
+var resultado = 0;
+var numero1 = 200;
+var numero2 = 300;
+
+resultado = numero1 + numero2;
+console.log("El resultado de la suma es: " + resultado);
+
+resultado = numero1 * numero2;
+console.log("El resultado de la mutiplcacion es: " + resultado);
+
+resultado = numero1 / numero2;
+console.log("El resultado de la división es: " + resultado);
+
+resultado = numero1 - numero2;
+console.log("El resultado de la resta es: " + resultado);
+
+// prompt: ingreso de datos
+var rol = prompt("Ingrese su rol: ");
+var direccion = prompt("Ingrese su domicilio: ");
+
+console.log("Su rol es: " + rol);
+console.log("Su domicilio es: " + direccion);
+
+// alert: ventana de dialogo
+alert("Cuidado con el covid");
+
